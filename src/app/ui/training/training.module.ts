d6fc9bb0216c6093d542d4f085a1d4bd3678438d
Ramprits <Rampritsahani@yahoo.com>
2017-12-05 23:00:00 +0530

    primeNg Added

--- src/app/ui/training/training.module.ts
+++ src/app/ui/training/training.module.ts
@@ -2,9 +2,31 @@ import { NgModule } from "@angular/core";
 import { TrainingListComponent } from "./training-list.component";
 import { RouterModule } from "@angular/router";
 import { TrainingRoutes } from "./training.routing";
-
+import { HttpClientModule } from "@angular/common/http";
+import {
+  InputTextModule,
+  DataTableModule,
+  PanelModule,
+  GrowlModule,
+  ButtonModule,
+  CheckboxModule,
+  DataGridModule
+} from "primeng/primeng";
+import { ReactiveFormsModule, FormsModule } from "@angular/forms";
 @NgModule({
-  imports: [RouterModule.forChild(TrainingRoutes)],
+  imports: [
+    RouterModule.forChild(TrainingRoutes),
+    InputTextModule,
+    PanelModule,
+    CheckboxModule,
+    GrowlModule,
+    DataGridModule,
+    ButtonModule,
+    ReactiveFormsModule,
+    FormsModule,
+    HttpClientModule,
+    DataTableModule
+  ],
   exports: [],
   declarations: [TrainingListComponent],
   providers: []

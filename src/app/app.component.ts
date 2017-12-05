21a8433b5149b6760db6b941541cae6b2b8f9f66
R1163213 <Ramprit.Sahani@ext.saint-gobain.com>
2017-12-05 19:11:07 +0530

    initial Update

---
+++ src/app/app.component.ts
@@ -0,0 +1,35 @@
+import { Component } from "@angular/core";
+import {
+  Router,
+  Event,
+  NavigationStart,
+  NavigationEnd,
+  NavigationCancel,
+  NavigationError
+} from "@angular/router";
+@Component({
+  selector: "mdb-root",
+  templateUrl: "app.component.html",
+  styleUrls: ["app.component.css"]
+})
+export class AppComponent {
+  loading: boolean;
+  constructor(router: Router) {
+    router.events.subscribe((routerEvent: Event) => {
+      this.checkRouterEvent(routerEvent);
+    });
+  }
+  checkRouterEvent(routerEvent: Event): void {
+    if (routerEvent instanceof NavigationStart) {
+      this.loading = true;
+    }
+
+    if (
+      routerEvent instanceof NavigationEnd ||
+      routerEvent instanceof NavigationCancel ||
+      routerEvent instanceof NavigationError
+    ) {
+      this.loading = false;
+    }
+  }
+}
